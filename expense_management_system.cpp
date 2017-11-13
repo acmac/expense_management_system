ece519eeedc829cb89f23827cd143c484520810f
Wangfangxin <2681086480@qq.com>
2017-11-13 21:26:05 +0800

    完成了增加收入和支出函数，实现了1-12月份收入支出的存储

--- expense_management_system.cpp
+++ expense_management_system.cpp
@@ -13,9 +13,275 @@ typedef struct
 {
 	char type[20]; //消费类型
 	float getSum;  //消费金额
+	float income;  //收入金额
 }person;
 
-person per[80];		//定义结构体数组
+person per[80];		
+
+//输入支出的信息
+int inputData(person sum[],int n)  
+{
+	int i=0;
+	char sign='y',x[10];
+
+	while(sign!='n'&&sign!='N')
+	{
+		cout<<"\t"<<"支出的种类"<<endl;
+		cin>>sum[n+i].type;  
+		cout<<"\t"<<"支出的金额(大于0)"<<endl;
+		cin>>sum[n+i].getSum;
+
+	//	gets(x);
+		cout<<"还需要再次输入吗？（y/n）";
+		cin>>sign;
+		i++;
+	}
+
+	cout<<"请输入收入的金额"<<endl;
+	cin>>sum[n+i].income;
+
+	return (n+i);
+}
+
+//储存数据
+void saveData(person sum[],int n)
+{
+	int i,month;
+	FILE *fp;
+	cout<<"\t"<<"请输入当前信息所属的月份:"<<endl;
+	cin>>month;
+	switch(month)
+	{
+	case 1:{
+			fp=fopen("D:\\一月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+
+
+	case 2:{
+			fp=fopen("D:\\二月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 3:{
+			fp=fopen("D:\\三月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;
+		   }
+		
+	case 4:{
+			fp=fopen("D:\\四月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 5:{
+			fp=fopen("D:\\五月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 6:{
+			fp=fopen("D:\\六月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 7:{
+			fp=fopen("D:\\七月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 8:{
+			fp=fopen("D:\\八月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 9:{
+			fp=fopen("D:\\九月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+		   }
+			
+	case 10:{
+			fp=fopen("D:\\十月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+			}
+			
+	case 11:{
+			fp=fopen("D:\\十一月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;	
+			}
+
+	case 12:{
+			fp=fopen("D:\\十二月份的支出信息.txt","a+"); //用append的方式写入文件
+			if(fp==NULL)
+			{
+				cout<<"\t\t"<<"the file can not be opened"<<endl;
+				exit(1);
+			}
+			fseek(fp,-1,SEEK_END);
+			for(i=0;i<n;i++)
+				fprintf(fp,"%-8s %-8f\n",sum[i].type,sum[i].getSum);
+			
+			char ss[20]="本月收入";
+			fprintf(fp,"%-8s %-8f\n",ss,sum[n].income);
+
+			fclose(fp);
+			cout<<"\t"<<"保存成功"<<endl;
+			break;
+			}
+	}
+}
 
 int menu_select()
 {
@@ -39,7 +305,7 @@ void main()//主函数
 {
 	int n=0,a=0; 
 	char c;
-	system("cls");
+	system("cls");  //清屏
 	  
     for(;;) 
 	{  
@@ -48,14 +314,14 @@ void main()//主函数
 		case 1:  
 			cout<<"\t"<<"请输入相应的收支情况"<<endl;
 
-//			n=InputItems(per,n);   //输入消费信息，食品消费，房租，子女教育费用，水电费，医疗费，储蓄等
-
+			n=inputData(per,n);   //输入消费信息，食品消费，房租，子女教育费用，水电费，医疗费，储蓄等
+			
 
 			cout<<"\t"<<"需要保存数据吗？ (y,n)"<<endl;
 			getchar();
 			cin>>c;
 			if(c=='y'||c=='Y')
-//			saveData();  //保存数据
+			saveData(per,n);  //保存数据
 			cout<<"\t";
 			system("pause");
 			break;
